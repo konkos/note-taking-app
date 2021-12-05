0283820f2742cabd0dc979bfb8627719753e1c32
kon kos <kon_kostas@windowslive.com>
2021-12-05 18:14:28 +0200

    code improvements

--- src/app/services/api-integration.service.ts
+++ src/app/services/api-integration.service.ts
@@ -8,32 +8,44 @@ import { ApiResponse } from '../models/response.model';
 export class ApiIntegrationService {
 
   url:String = 'https://v2.jokeapi.dev/joke/'
-  categoriesLIST:String[] = ['Programming','Miscellaneous','Dark','Pun','Spooky','Christmas']
+  public categoriesLIST:String[] = ['Programming','Miscellaneous','Dark','Pun','Spooky','Christmas']
   blackListFlagsLIST = ['nsfw','religious','political','racist','sexist','explicit']
   constructor(private http:HttpClient) { }
 
-  getApiResult(categories:String[], blackListFlags:String[]){    
+  getApiResult(categories:String[], blackListFlags:String[]){
+    
+    this.addCategoriesIntoURL(categories)
+    this.addBlackListFlagsIntoURL(blackListFlags)
 
-    if(categories.length > 0){
-      categories.map(category => this.url += `${category},`)
-      this.url = this.url.slice(0,-1) //remove last comma
-      console.log(`URL1:::  ${this.url}`);      
-    }
+    console.log(`URL2:::  ${this.url}`);
+    
+    return this.http
+    .get<ApiResponse>(`${this.url}`,{observe:'body'})
+  }
 
+  private removeLastComma(str:String):String {
+    return str.slice(0,-1)
+  }
+
+  private addCategoriesIntoURL(categories:String[]) {
+    if(categories.length <= 0){ return }
+
+    categories.map(category => this.url += `${category},`)
+    this.url = this.removeLastComma(this.url)
+    console.log(`URL1:::  ${this.url}`);      
+  }
+
+  private addBlackListFlagsIntoURL(blackListFlags:String[]) {
     if(blackListFlags.length > 0){
       this.url += "?blacklistFlags="
       blackListFlags.map(flag => this.url += `${flag},`)
-      this.url = this.url.slice(0,-1) //remove last comma
+      this.url = this.removeLastComma(this.url)
       //last step to get a single joke
       this.url += '&type=single'
     }else{
       //last step to get a single joke
       this.url += '?type=single'
     }
-    console.log(`URL2:::  ${this.url}`);
-    
-    return this.http
-    .get<ApiResponse>(`${this.url}`,{observe:'body'})
   }
 
 }
